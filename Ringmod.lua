9ce1e14ec93f76b2e188655633550db9806917b4
SuperNiCd <jfilbrun@cinci.rr.com>
2018-02-24 15:38:05 -0500

    working unit!

--- Ringmod.lua
+++ Ringmod.lua
@@ -12,23 +12,69 @@ local Ringmod = Class{}
 Ringmod:include(Unit)
 
 function Ringmod:init(args)
-  args.title = "FM Op"
-  args.mnemonic = "FM"
-  args.version = 2
+  args.title = "Ring Mod"
+  args.mnemonic = "RM"
+  args.version = 1
   Unit.init(self,args)
 end
 
 function Ringmod:onLoadGraph(pUnit,channelCount)
- 
+ -- input, vca, sine osc
+
+ -- create sine osc
+ local modulator = self:createObject("SineOscillator","modulator")
+
+ -- create multipliers
+ local mult1 = self:createObject("Multiply","mult1")
+ local mult2 = self:createObject("Multiply","mult2")
+
+ -- create f0 gainbias & minmax
+ local f0 = self:createObject("GainBias","f0")
+ local f0Range = self:createObject("MinMax","f0Range")
+
+ -- connect unit input to vca/multipler
+ connect(pUnit,"In1",mult1,"Left")
+ if channelCount > 1 then
+    connect(pUnit,"In2",mult2,"Left")
+ end 
+
+ -- connect vca/multiplier to unit output
+ connect(mult1,"Out",pUnit,"Out1")
+ if channelCount > 1 then
+    connect(mult2,"Out",pUnit,"Out2")
+ end 
+
+-- connect sine osc to right Inlet of vca/multiplier
+connect(modulator,"Out",mult1,"Right")
+if channelCount > 1 then
+    connect(modulator,"Out",mult2,"Right")
+end 
+
+connect(f0,"Out",modulator,"Fundamental")
+connect(f0,"Out",f0Range,"In")
+
+self:addBranch("f0","Fundamental",f0,"In")
+
 end
 
 local views = {
-  expanded = {"tune","freq","num","den","index","level"},
+  expanded = {"freq"},
   collapsed = {},
 }
 
 function Ringmod:onLoadViews(objects,controls)
-
+    controls.freq = GainBias {
+        button = "f0",
+        description = "Fundamental",
+        branch = self:getBranch("Fundamental"),
+        gainbias = objects.f0,
+        range = objects.f0Range,
+        biasMap = Encoder.getMap("oscFreq"),
+        biasUnits = app.unitHertz,
+        initialBias = 200.0,
+        gainMap = Encoder.getMap("freqGain"),
+        scaling = app.octaveScaling
+      }
   return views
 end
 
---
+++ init.lua
@@ -0,0 +1,13 @@
+local units = {
+    {category="Experimental"},
+    {title="Ring Modulator",moduleName="Ringmod",keywords="pitch, modulate"},
+  }
+  
+  return {
+    title = "Ring Mod",
+    name = "ringmod",
+    contact = "Joe",
+    keyword = "test",
+    units = units
+  }
+  
